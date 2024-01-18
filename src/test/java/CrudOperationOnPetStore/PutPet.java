5069684311a801e0fe5741412e902205441ed0f7
Varunsn9 <varunsn9@gmail.com>
2024-01-18 11:00:45 +0530

    changes in PutPost

--- src/test/java/CrudOperationOnPetStore/PutPet.java
+++ src/test/java/CrudOperationOnPetStore/PutPet.java
@@ -17,45 +17,21 @@ public class PutPet {
 public void putPet() {
 	baseURI="https://petstore.swagger.io/v2";
 	JSONObject userJsonObject=new JSONObject();
-	userJsonObject.put("id", 0);
+	userJsonObject.put("id", 121);
     userJsonObject.put("username", "va");
     userJsonObject.put("firstName", "he");
     userJsonObject.put("lastName", "aa");
     userJsonObject.put("email", "asd@gmail.com");
     userJsonObject.put("password", "aassddffgg");
-    userJsonObject.put("phone", "9874563210");
-    userJsonObject.put("userStatus", 0);
+    userJsonObject.put("phone", "9844563210");
+    userJsonObject.put("userStatus", 12);
 	
-    given().body(userJsonObject)
-	.when().put("/user")
+    given().body(userJsonObject).contentType(ContentType.JSON)
+	.when().put("/user/va")
 	.then()
 	.statusCode(200)
 	.contentType(ContentType.JSON)
 	.log()
 	.all();
 	
-}
-public JSONObject createPetJsonObject() {
-    JSONObject petJsonObject = new JSONObject();
-    int number=new Random().nextInt(1000);
-    petJsonObject.put("id", number);
-
-    JSONObject category = new JSONObject();
-    category.put("id", 0+number);
-    category.put("name", "string");
-    petJsonObject.put("category", category);
-
-    petJsonObject.put("name", "daoggie"+number);
-
-    petJsonObject.put("photoUrls",new String[]{"string"});
-
-    JSONObject tag = new JSONObject();
-    tag.put("id", 0);
-    tag.put("name", "aang"+number);
-    petJsonObject.put("tags", new JSONObject[]{tag});
-
-    petJsonObject.put("status", "available");
-
-    return petJsonObject;
-}
-}
+}}
